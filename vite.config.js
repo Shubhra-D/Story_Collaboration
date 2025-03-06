da1c10f289ba5dad30b60cd2ba761416cf365c7e
Shubhra Dwivedi <shubhradwivedi21@gmail.com>
2025-03-06 23:19:27 +0530

    still the .env file issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define:{
+     'import.meta.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
+    'import.meta.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
+    'import.meta.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
+    'import.meta.env.VITE_FIREBASE_DATABASE_URL':JSON.stringify.apply(process.env.VITE_FIREBASE_DATABASE_URL),
+    'import.meta.env.VITE_FIREBASE_STORAGE_BUCKET':JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET),
+    'import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID':JSON.stringify(process.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+    'import.meta.env.VITE_FIREBASE_APP_ID':JSON.stringify(process.env.VITE_FIREBASE_APP_ID),
+  },
   base:"/Story_Collaboration",
   plugins: [react(), tsconfigPaths()],
 })
